dd31a61220779f96adfc5f49bdc5c9eaf951eca9
github username <github email address>
2020-05-14 02:32:58 +0100

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,10 @@ class ApplicationController < Sinatra::Base
   	set :public_dir, "public"
   end
 
+  #Responds with a 200 status code
+  #Renders index.erb
   get "/" do
   	erb :index
   end
+  
 end
